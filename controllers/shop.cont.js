402ae87ac59d058c8328b36639b2ce3954e14f13
ezdall <67018806+ezdall@users.noreply.github.com>
2023-11-03 15:59:59 +0800

    add controllers / crud for shop

--- controllers/shop.cont.js
+++ controllers/shop.cont.js
@@ -1,51 +1,199 @@
+const mongoose = require('mongoose');
+const path = require('path')
+const fs = require('fs');
+const _extend = require('lodash/extend');
+const { IncomingForm } = require('formidable');
 
+const Shop = require('../models/shop.model')
 
+// const defaultImage = require('../dist/img/default.jpg')
+// import defaultImage from '../dist/img/default.jpg';
 
-
+// helper
+const { BadRequest400 } = require('../helpers/bad-request.error');
+const { Unauthorized401 } = require('../helpers/unauthorized.error');
+const { Forbidden403 } = require('../helpers/forbidden.error')
+const { NotFound404 } = require('../helpers/not-found.error');
 
 
 const list = async (req, res, next) => {
-	return res.json('shop-list')
+	 try {
+    const shops = await Shop.find().lean().exec()
+
+    if(!shops?.length) return next(new NotFound404('no shops @list-shop'))
+
+    res.json(shops)
+  } catch (err){
+    return next(err)
+  }
 }
 
 const listByOwner = async (req, res, next) => {
-	return res.json('listByOwner')
+	try {
+    const shops = await Shop.find({ owner: req.profile._id})
+     .populate({path:'owner', select:'name'})
+     .lean().exec()
+   
+    if(!shops?.length) return next(new NotFound404('no shops @list-by-owner'))
+
+    res.json(shops)
+  } catch (err){
+    return res.status(400).json({
+      error: errorHandler.getErrorMessage(err)
+    })
+  }
 }
 
-const read = async (req, res, next) => {
-	return res.json(`read ${req.params.shopId}`)
+const read = (req, res, next) => {
+
+  if(!req.shop) return next(new NotFound404('no shop'))
+
+	const shop = req.shop.toObject()
+
+	shop.image = undefined
+
+  return res.json(shop)
 }
 
-const create = async (req, res,next) => {
-	return res.json('create')
+const create = (req, res,next) => {
+
+// only allowed 'multipar/form-data' (formidable)
+// reject 'json' and 'form-urlencoded'
+ if(!req.is('multipart/form-data')){
+  return next(new BadRequest400('invalid form'))
+ }
+
+	const form = new IncomingForm({ 
+      keepExtensions: true,
+      maxFileSize: 2*1024*1024, // 2mb
+     })
+
+  form.parse(req, async (err, fields, files) => {
+    	if (err) return next(err)
+try {
+    const shop = new Shop(fields)
+
+    shop.owner = req.profile._id
+
+    if(files.image){
+      if(files.image?.size > 1000000){
+      	return next(new BadRequest400('max 2mb image size '))
+      }
+      shop.image.data = fs.readFileSync(files.image.path)
+      shop.image.contentType = files.image.type
+    }
+    
+      const result = await shop.save()
+      return res.status(201).json(result)
+    }catch (error){
+      return next(error)
+    }
+  })
 }
 
 const update = async (req, res,next) => {
-return res.json(`upd ${req.params.shopId}`)
+
+  if(!req.is('multipart/form-data')){
+    return next(new BadRequest400('invalid form @updShop'))
+   }
+
+	const form = new IncomingForm({
+    keepExtensions: true,
+    maxFileSize: 2*1024*1024 // 2mb
+  })
+
+  return form.parse(req, async (err, fields, files) => {
+   if (err) return next(err);
+ 	
+ 	try {
+    let { shop } = req
+    
+    shop = _extend(shop, fields)
+    
+    if(files.image){
+      shop.image.data = fs.readFileSync(files.image.path)
+      shop.image.contentType = files.image.type
+    }
+
+     const result = await shop.save()
+
+     if(!result){
+	 		return next(new BadRequest400('invalid update @updShop'))
+	 	 }
+
+      return res.json(result)
+    }catch (error){
+      return next(error)
+    }
+  })
 }
 
 const remove = async (req, res,next) => {
-	return res.json(`delete ${req.params.shopId}`)
+	 try {
+    const { shop}  = req
+
+    if(!shop) return next(new NotFound404('shop not found @delShop'))
+
+    const deletedShop = shop.deleteOne()
+
+    res.json(deletedShop)
+  } catch (err) {
+    return next(err)
+  }  
 }
 
-const photo = async (req, res,next) => {
-	console.log('photo')
+const isOwner = (req, res, next) => {
+	// const { shop, auth } = req
 
-	return next()
-}
+	// const isOwner = shop && auth && shop.owner._id == auth._id
+  
+ //  if(!isOwner){
+ //    return next(new Forbidden403('forbidden! not owner'))
+ //  }
 
-const defaultPhoto = async (req, res,next) => {
-	return res.json('defaultPhoto')
-}
-
-const isOwner = async (req, res, next) => {
 	console.log('isOwner')
 	return next()
 }
 
-const shopById = async (req, res, next) => {
-	console.log('shopById')
-	return next()
+const shopById = async (req, res, next, shopId) => {
+	 try {
+ 	 	if(!shopId || !mongoose.isValidObjectId(shopId)){
+	 		return next(new BadRequest400('valid id is required @shopById'))
+	 	}
+
+    const shop = await Shop.findById(shopId)
+    		.populate({ path:'owner', select:'name' }).exec()
+   
+    if (!shop) return next(new NotFound404("Shop not found"))
+  
+    // mount
+    req.shop = shop
+    // must
+    next()
+
+  } catch (err) {
+    return next(err)
+  }
+}
+
+const photo = (req, res,next) => {
+	const { image } = req.shop;
+
+ 	if(image?.data){
+    res.set("Content-Type", image.contentType)
+    return res.send(image.data)
+  }
+
+  console.log('no-photo')
+
+  return next()
+}
+
+const defaultPhoto = async (req, res,next) => {
+  // console.log(`${process.cwd()}${defaultImage}`)
+  // console.log(process.cwd())
+	 // return res.sendFile(path.resolve)
+    res.sendFile(path.resolve(__dirname, '..', 'dist', 'img', 'default.jpg'))
 }
 
 module.exports = { 
